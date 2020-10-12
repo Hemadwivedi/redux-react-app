e246029e73641e0f09937f488ba63f6af4b7ce9f
HDwivedi <hemaasheesh@gmail.com>
2020-10-12 18:40:42 -0400

    added all files

---
+++ counterapp/redux-basics.js
@@ -0,0 +1,35 @@
+const Redux=require("redux");
+const createStore=Redux.createStore;
+const initialState={
+    counter:0
+}
+
+//Reducer
+const rootReducer=(state=initialState,action)=>{
+    if(action.type==='INC_COUNTER'){
+        return{
+            ...state,
+            counter:state.counter+1
+        }
+        
+        
+    
+    
+    }    if(action.type==='ADD_COUNTER'){
+return{
+    ...state,
+    counter:state.counter+action.value
+}
+        
+    }
+    return state;
+}
+
+//store
+
+const store=createStore(rootReducer);
+console.log(store.getState());
+
+store.dispatch({type:"INC_COUNTER"});
+store.dispatch({type:"ADD_COUNTER",value:10});
+console.log(store.getState());
